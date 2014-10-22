b47d085fc063a7d6e8cce7b91b5f13e75e7c48ee
glenn <glennlefevere@hotmail.com>
2014-10-22 15:58:40 +0200

    met testclass in java test

--- src/main/java/be/vdab/services/EmployeeService.java
+++ src/main/java/be/vdab/services/EmployeeService.java
@@ -10,9 +10,26 @@ import be.vdab.entities.Employee;
  *
  */
 public interface EmployeeService {
+	/**
+	 * Het wegschrijven van een werknemer naar de database
+	 * @param employee
+	 */
 	public void create(Employee employee);
+	/**
+	 * Haalt alle werknemers op uit de database
+	 * @return een lijst met werknemers
+	 */
 	public List<Employee> findAll();
+	/**
+	 * Zoekt een werknemer op basis van een id
+	 * @param id 
+	 * @return
+	 */
 	public Employee find(Long id);
+	/**
+	 * 
+	 * @param employee
+	 */
 	public void remove(Employee employee);
 	public void edit(Employee employee);
 	public List<Employee> findByNaamContaining(String waarde);
